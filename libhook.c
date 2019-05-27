8d6f0530f295de38527c9a172ec38708f1fd043f
wasabbi <lee.yeb33@gmail.com>
2019-05-27 17:54:39 +0800

    update for backup orig_cpu_set

--- libhook.c
+++ libhook.c
@@ -7,8 +7,6 @@
 
 typedef int (*orig_pthread_create_ftype)(pthread_t *, const pthread_attr_t *, void* (void *), void *);
 
-int exec_count = 0;
-
 void* thread1 = 0x4009fb;
 void* thread2 = 0x400ab4;
 
@@ -17,26 +15,49 @@ int pthread_create(pthread_t *__restrict __newthread,
 			   void *(*__start_routine) (void *),
 			   void *__restrict __arg){
 
-    
-    if(__start_routine == thread1){
-        cpu_set_t mask;
-        CPU_ZERO(&mask);
-        CPU_SET(0, &mask);  //binding to CPU0
-        if (pthread_setaffinity_np(pthread_self(),sizeof(mask),&mask) < 0)
-            fprintf(stderr,"A: set thread affinity failed\n");
-        printf("%x binds to CPU1\n", pthread_self());
-    }else if(__start_routine == thread2){
-        cpu_set_t mask;
-        CPU_ZERO(&mask);
-        CPU_SET(1, &mask);  //binding to CPU1
-        if (pthread_setaffinity_np(pthread_self(),sizeof(mask),&mask) < 0)
-            fprintf(stderr,"B: set thread affinity failed\n");
-        printf("%x binds to CPU1\n", pthread_self());
-    }
-    
+    int ret;
+
     orig_pthread_create_ftype orig_pthread_create;
     orig_pthread_create = (orig_pthread_create_ftype)dlsym(RTLD_NEXT, "pthread_create");
+
+    if(__start_routine == thread1){
+        int j;
+        cpu_set_t orig_cpu_set;
+        CPU_ZERO(&orig_cpu_set);
+        if (pthread_getaffinity_np(pthread_self(),sizeof(orig_cpu_set),&orig_cpu_set) < 0 )   //backup orig_cpu_set
+            fprintf(stderr,"get thread affinity failed\n");
+            
+        cpu_set_t mask;
+        CPU_ZERO(&mask);
+        CPU_SET(0, &mask);
+        if (pthread_setaffinity_np(pthread_self(),sizeof(mask),&mask) < 0)                  //bind the thread to CPU0
+            fprintf(stderr,"set thread affinity failed\n");
+        printf("Thread1 binds to CPU1\n");
+        ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+        
+        if (pthread_setaffinity_np(pthread_self(),sizeof(orig_cpu_set),&orig_cpu_set) < 0)  //recover orig_cpu_set
+            fprintf(stderr,"set thread affinity failed\n");
+    }else if(__start_routine == thread2){
+        int j;
+        cpu_set_t orig_cpu_set;
+        CPU_ZERO(&orig_cpu_set);
+        if (pthread_getaffinity_np(pthread_self(),sizeof(orig_cpu_set),&orig_cpu_set) < 0 )   //backup orig_cpu_set
+            fprintf(stderr,"get thread affinity failed\n");
+            
+        cpu_set_t mask;
+        CPU_ZERO(&mask);
+        CPU_SET(1, &mask);
+        if (pthread_setaffinity_np(pthread_self(),sizeof(mask),&mask) < 0)                  //bind the thread to CPU1
+            fprintf(stderr,"set thread affinity failed\n");
+        ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+        printf("Thread2 binds to CPU1\n");
+        
+        if (pthread_setaffinity_np(pthread_self(),sizeof(orig_cpu_set),&orig_cpu_set) < 0)  //recover orig_cpu_set
+            fprintf(stderr,"set thread affinity failed\n");
+    }else{
+        ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+    }
     
-    return (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+    return ret;
 }
 
